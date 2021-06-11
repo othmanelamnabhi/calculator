f383cc48558a34c6f50e73ce606d4a589ce39a49
Othmane Lamnabhi <23281797+othmanelamnabhi@users.noreply.github.com>
2021-06-11 19:01:14 +0100

    create Calculator class with some setters and encompass almost all calculator logic to its own file

--- calculator.js
+++ calculator.js
@@ -1,16 +1,95 @@
-// create class with following methods
-// clear
-// delete
-// add
-// substract
-// multiply
-// divide
+export const primaryOperand = document.querySelector("[data-primary-operand]");
+export const secondaryOperand = document.querySelector(
+  "[data-secondary-operand]"
+);
+export const operationSign = document.querySelector("[data-operation-sign]");
+export const thousandSeparator = /(?<!\.\d*)(\d{1,3}(?=(\d{3})+(?!\d)))/g;
 
-class Calculator {
-  static clearAll() {}
-  static delete() {}
-  static add() {}
-  static substract() {}
-  static multiply() {}
-  static divide() {}
+export class Calculator {
+  static get primaryOperand() {
+    return primaryOperand.dataset.primaryOperand;
+  }
+
+  static set primaryOperand(value) {
+    if (primaryOperand.textContent === "0") {
+      primaryOperand.textContent = value;
+      primaryOperand.dataset.primaryOperand = value;
+    } else {
+      primaryOperand.dataset.primaryOperand += value;
+      primaryOperand.textContent =
+        primaryOperand.dataset.primaryOperand.replaceAll(
+          thousandSeparator,
+          "$&,"
+        );
+    }
+  }
+
+  static get secondaryOperand() {
+    return secondaryOperand.dataset.secondaryOperand;
+  }
+
+  static set secondaryOperand(value) {
+    secondaryOperand.textContent = value.replaceAll(thousandSeparator, "$&,");
+    secondaryOperand.dataset.secondaryOperand = value;
+  }
+
+  static get operationSign() {
+    return operationSign.dataset.operationSign;
+  }
+
+  static set operationSign(value) {
+    operationSign.textContent = value;
+    operationSign.dataset.operationSign = value;
+  }
+
+  static clearAll() {
+    primaryOperand.textContent = 0;
+    primaryOperand.dataset.primaryOperand = "";
+    secondaryOperand.textContent = "";
+    secondaryOperand.dataset.secondaryOperand = "";
+    operationSign.textContent = "";
+    operationSign.dataset.operationSign = "";
+  }
+  // Delete the last digit from the primaryOperand and set it to 0 if empty
+  static delete() {
+    primaryOperand.dataset.primaryOperand =
+      primaryOperand.dataset.primaryOperand.slice(0, -1);
+    primaryOperand.textContent =
+      primaryOperand.dataset.primaryOperand.replaceAll(
+        thousandSeparator,
+        "$&,"
+      );
+    if (primaryOperand.textContent.length === 0) {
+      this.primaryOperand = 0;
+    }
+  }
+  // Find the operation sign clicked and run the matching operation
+  static equals(sign) {
+    switch (sign) {
+      case "*":
+        return (
+          parseFloat(primaryOperand.dataset.primaryOperand) *
+          parseFloat(secondaryOperand.dataset.secondaryOperand)
+        );
+        break;
+      case "÷":
+        return (
+          parseFloat(secondaryOperand.dataset.secondaryOperand) /
+          parseFloat(primaryOperand.dataset.primaryOperand)
+        );
+        break;
+      case "+":
+        return (
+          parseFloat(secondaryOperand.dataset.secondaryOperand) +
+          parseFloat(primaryOperand.dataset.primaryOperand)
+        );
+        break;
+      case "-":
+        return (
+          parseFloat(secondaryOperand.dataset.secondaryOperand) -
+          parseFloat(primaryOperand.dataset.primaryOperand)
+        );
+        break;
+    }
+  }
 }
--- script.js
+++ script.js
@@ -1,54 +1,65 @@
-const primaryOperand = document.querySelector("[data-primary-operand]");
-const secondaryOperand = document.querySelector("[data-secondary-operand]");
-const operationSign = document.querySelector("[data-operation]");
-
-const thousandSeparator = /(?<!\.\d*)(\d{1,3}(?=(\d{3})+(?!\d)))/g;
-
-console.log(primaryOperand);
-console.log(secondaryOperand);
-console.log(operationSign);
+import {
+  Calculator,
+  primaryOperand,
+  secondaryOperand,
+  operationSign,
+  thousandSeparator,
+} from "./calculator.js";
 
 // Calculator displays 0 initially
 primaryOperand.textContent = 0;
 
+// add event listener on the whole document then filter by button clicked
 document.addEventListener("click", (e) => {
+  // if number is clicked
   if (e.target.hasAttribute("data-number")) {
     console.log("number");
-    // digit is inputed in calculator screen (if 0 in the beginning replace // else add number at the end)
-    if (primaryOperand.textContent === "0") {
-      primaryOperand.textContent = e.target.textContent;
-      primaryOperand.dataset.primaryOperand = e.target.textContent;
-    } else {
-      primaryOperand.dataset.primaryOperand =
-        primaryOperand.dataset.primaryOperand + e.target.textContent;
+    Calculator.primaryOperand = e.target.textContent;
+  }
+  // if operation sign is clicked
+  if (e.target.hasAttribute("data-operation")) {
+    console.log("operation");
+    // check if operationSign and secondaryOperand are empty so we can assign values to them then set primaryOperand to 0
+    if (
+      operationSign.textContent === "" &&
+      secondaryOperand.textContent === ""
+    ) {
+      Calculator.secondaryOperand = primaryOperand.dataset.primaryOperand;
+      Calculator.operationSign = e.target.textContent;
+      primaryOperand.dataset.primaryOperand = 0;
+      primaryOperand.textContent = 0;
+    }
+  }
+  // if delete is clicked
+  if (e.target.hasAttribute("data-delete")) {
+    console.log("delete");
+    Calculator.delete();
+  }
+  // if equal sign is clicked
+  if (e.target.hasAttribute("data-equals")) {
+    console.log("equals");
+    // run this only if all 3 fields have values
+    if (
+      primaryOperand.dataset.primaryOperand !== "" &&
+      secondaryOperand.dataset.secondaryOperand !== "" &&
+      operationSign.dataset.operationSign !== ""
+    ) {
+      primaryOperand.dataset.primaryOperand = Calculator.equals(
+        operationSign.textContent
+      );
       primaryOperand.textContent =
         primaryOperand.dataset.primaryOperand.replaceAll(
           thousandSeparator,
           "$&,"
         );
+      // clear all fields when result is calulated
+      Calculator.secondaryOperand = "";
+      Calculator.operationSign = "";
     }
-    // check if need to add thousand separator after each number input
-    // add separator to number if applicable
   }
-  if (e.target.hasAttribute("data-operation")) {
-    console.log("operation");
-    // if operationSign and secondaryOperand are null
-    //  take primaryOperand and put is as secondaryOperand
-    //  take the sign and add it as operationSign
-  }
-  if (e.target.hasAttribute("data-delete")) {
-    console.log("delete");
-    // pop the last digit from primaryOperand (careful with separators)
-    // when all empty output O
-  }
-  if (e.target.hasAttribute("data-equals")) {
-    console.log("equals");
-    // edge case when there is no secondary operand
-  }
+  // if clear is clicked
   if (e.target.hasAttribute("data-all-clear")) {
     console.log("all-clear");
-    // clear primaryOperand
-    // clear secondaryOperand
-    // clear operationSign
+    Calculator.clearAll();
   }
 });
